3b6293f4237e7820612cb48a396dcec9b243833a
ninum12 <ninum@sookmyung.ac.kr>
2025-02-03 14:35:15 +0900

    Feat: 술 목록 데이터 작성

---
+++ src/database/AlcoholDisplayList.js
@@ -0,0 +1,38 @@
+export const ALCOHOL_DISPLAY_LIST = [
+  {
+    nameKO: "처음처럼",
+    img: "",
+  },
+  {
+    nameKO: "진로",
+    img: "",
+  },
+  {
+    nameKO: "별빛청하 스파클링",
+    img: "",
+  },
+  {
+    nameKO: "테라",
+    img: "",
+  },
+  {
+    nameKO: "버드 와이저",
+    img: "",
+  },
+  {
+    nameKO: "아사히",
+    img: "",
+  },
+  {
+    nameKO: "두르뜨, 그랑 떼루아",
+    img: "",
+  },
+  {
+    nameKO: "디코이 샤도네이",
+    img: "",
+  },
+  {
+    nameKO: "브레드&버터",
+    img: "",
+  },
+];
